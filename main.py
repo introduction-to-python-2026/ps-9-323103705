d78226a5347bbd72dc4385d10c856e9917060485
323103705 <rotemr2001@gmail.com>
2025-12-26 15:07:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
 
+df = pd.read_csv("parkinsons.csv")
+
+x = df[['DFA', 'HNR', 'RPDE', 'PPE']]
+y = df[['status']]
+
+sns.pairplot(df, vars=['DFA', 'HNR', 'RPDE', 'PPE'], hue='status')
+plt.show()
+
+scaler = MinMaxScaler()
+x_scale = scaler.fit_transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x_scale, y, test_size=0.2)
+
+dt = SVC(kernel='linear')
+dt.fit(x_train, y_train)
+
+y_predict = dt.predict(x_test)
+accuracy = accuracy_score(y_test, y_predict)
+
+joblib.dump(dt, 'parkinsons.joblib')
+
+dt.fit(x_train, y_train)
